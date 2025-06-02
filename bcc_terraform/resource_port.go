a4b3e9520c10dae961bb6b1eea5fa6c7a28b729d
Egor.penteleychuk <egor.penteleychuk@axenix.pro>
2025-06-02 17:16:50 +0300

    update: task-3164. Add new inject with optional vdc_id for resource port

--- bcc_terraform/resource_port.go
+++ bcc_terraform/resource_port.go
@@ -14,7 +14,7 @@ import (
 
 func resourcePort() *schema.Resource {
 	args := Defaults()
-	args.injectContextVdcById()
+	args.injectContextOptionalVdcById()
 	args.injectCreatePort()
 
 	return &schema.Resource{
